11fb366743286152eb0631009d50c12946f3360b
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-08-23 17:10:08 -0500

    created Xcode project

---
+++ Xcode/libb64.h
@@ -0,0 +1,21 @@
+//
+//  libb64.h
+//  libb64
+//
+//  Created by Alsey Coleman Miller on 8/23/15.
+//  Copyright © 2015 Chris Venter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libb64.
+FOUNDATION_EXPORT double libb64VersionNumber;
+
+//! Project version string for libb64.
+FOUNDATION_EXPORT const unsigned char libb64VersionString[];
+
+#import <libb64/cencode.h>
+#import <libb64/cdecode.h>
+
+
+
